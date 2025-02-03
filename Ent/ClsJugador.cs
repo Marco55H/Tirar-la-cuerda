e4cabbc3a34243d95be9bb7f3bb8b3441621bf65
Marco55H <marco.holguin@iesnervion.es>
2025-02-03 14:40:41 +0100

    ey

---
+++ Ent/ClsJugador.cs
@@ -0,0 +1,29 @@
+﻿namespace Ent
+{
+    public class ClsJugador
+    {
+        #region atributos
+        private string nombre="";
+
+        private string grupo ="";
+
+        private int puntuacion = 0;
+        #endregion
+
+        #region Propiedades
+        public string Nombre {  get { return nombre; } set { nombre = value; } }
+        public string Grupo { get { return grupo; } set { grupo = value; } }
+        public int Puntuacion { get { return puntuacion; } set { puntuacion = value; } }
+        #endregion
+
+        #region Constructores
+        public ClsJugador() { }
+        public ClsJugador(string _nombre, string _grupo, int _puntuacion)
+        {
+            this.nombre = _nombre;
+            this.grupo = _grupo;
+            this.puntuacion = _puntuacion;
+        }
+        #endregion
+    }
+}
